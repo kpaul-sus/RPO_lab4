fbbe5fcce9a0f1aca484318771494d6ef64ed4d0
Dmitriy Punegov <punegov02@gmail.com>
2025-04-27 20:01:05 +0300

    done feature1

--- main.py
+++ main.py
@@ -3,12 +3,22 @@ def main():
     file = open('result.txt', 'w')
     a = int(input("Введите первый операнд: "))
     b = int(input("Введите второй операнд: "))
+
     print("Сложение")
     file.write(str(add(a, b)))
+    file.write('\n')
+
+    print("Умножение")
+    file.write(str(mult(a, b)))
+    file.write('\n')
     
 
 def add( a, b ):
     res = a + b
+    return res
+
+def mult( a, b ):
+    res = a * b
     return res
 
 if __name__ == "__main__":
