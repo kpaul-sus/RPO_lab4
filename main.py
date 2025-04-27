3f28954c1fe6578347d303ff7b8a568d14beb8a3
volkow-symeon <volckow.semen20@yandex.ru>
2025-04-27 20:17:06 +0300

    done feature2 (#2)

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ def main():
     file = open('result.txt', 'w')
     a = int(input("Введите первый операнд: "))
     b = int(input("Введите второй операнд: "))
-
-    print("Сложение")
+    
+    file.write("Сложение\n")
     file.write(str(add(a, b)))
     file.write('\n')
+    
+    file.write("Вычитание\n")
+    file.write(str(sub(a, b)))
+    file.write('\n')
 
-    print("Умножение")
+    file.write("Умножение\n")
     file.write(str(mult(a, b)))
     file.write('\n')
     
@@ -18,6 +22,9 @@ def add( a, b ):
 
 def mult( a, b ):
     return a * b
+
+def sub( a, b ):
+    return a - b
 
 if __name__ == "__main__":
     main()
